0ffa3254369f6dfe3ec7c6e67aed3eabd7691509
Swiss Creation <contact@swiss-creation.com>
2013-06-13 22:16:28 +0200

    New features

---
+++ TestGitHub/TestGitHub/NSObject+TestObject.h
@@ -0,0 +1,13 @@
+//
+//  NSObject+TestObject.h
+//  TestGitHub
+//
+//  Created by Sébastien Lauper on 6/13/13.
+//  Copyright (c) 2013 Swiss Creation. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSObject (TestObject)
+
+@end
---
+++ TestGitHub/TestGitHub/NSObject+TestObject.m
@@ -0,0 +1,13 @@
+//
+//  NSObject+TestObject.m
+//  TestGitHub
+//
+//  Created by Sébastien Lauper on 6/13/13.
+//  Copyright (c) 2013 Swiss Creation. All rights reserved.
+//
+
+#import "NSObject+TestObject.h"
+
+@implementation NSObject (TestObject)
+
+@end
